c66bece8a028e30c7fb2c03eb9e47f24f35cd15b
hrvieira <luizhrvieira@outlook.com>
2023-06-22 16:42:40 -0300

    Opção de cadastramento concluído e com suas correções

--- controller/cadastraClientes-controller.js
+++ controller/cadastraClientes-controller.js
@@ -1,5 +1,14 @@
-const formulario = document.querySelector('[data-form')
+import { clienteService } from "../service/cliente-service.js";
 
-addEventListener('submit', () => {
-  
+const formulario = document.querySelector('[data-form]')
+
+addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  const nome = evento.target.querySelector('[data-nome]').value;
+  const email = evento.target.querySelector('[data-email]').value;
+
+  clienteService.criaCliente(nome, email)
+  .then(() => {
+    window.location.href = '../telas/cadastro_concluido.html'
+  })
 })
