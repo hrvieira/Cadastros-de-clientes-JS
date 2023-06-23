dae703a34839d90e0a330d825bad7f634a16bf37
hrvieira <luizhrvieira@outlook.com>
2023-06-23 14:30:31 -0300

    Desafio e projeto final concluído. Desafio era adicionar async/await e try/catch à página cadastraClientes-controller.js

--- controller/cadastraClientes-controller.js
+++ controller/cadastraClientes-controller.js
@@ -2,13 +2,18 @@ import { clienteService } from "../service/cliente-service.js";
 
 const formulario = document.querySelector('[data-form]')
 
-addEventListener('submit', (evento) => {
+// quando há um evento, o async entra após a ação e deixa async o que evento vai fazer
+addEventListener('submit', async (evento) => {
   evento.preventDefault();
-  const nome = evento.target.querySelector('[data-nome]').value;
-  const email = evento.target.querySelector('[data-email]').value;
-
-  clienteService.criaCliente(nome, email)
-  .then(() => {
+  try {
+    const nome = evento.target.querySelector('[data-nome]').value;
+    const email = evento.target.querySelector('[data-email]').value;
+  
+    await clienteService.criaCliente(nome, email)
     window.location.href = '../telas/cadastro_concluido.html'
-  })
+  }
+  catch(erro){
+    console.log(erro)
+    window.location.href = '../telas/erro.html'
+  }
 })
